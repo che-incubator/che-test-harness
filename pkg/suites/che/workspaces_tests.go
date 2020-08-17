16e0621d660ff480806395912622f47142ed3b15
Flavius Lacatusu <flacatus@redhat.com>
2020-08-18 00:25:57 +0200

    Update workspaces_tests.go

--- pkg/suites/che/workspaces_tests.go
+++ pkg/suites/che/workspaces_tests.go
@@ -16,48 +16,6 @@ var _ =  ginkgo.Describe( "[Workspaces]" , func() {
 		panic("Failed to create zap logger")
 	}
 
-	ginkgo.It("Start simple Workspace", func() {
-		workspaceStack := "simple"
-		httpClient, err := client.NewHttpClient()
-
-		ctrl := workspaces.NewWorkspaceController(httpClient)
-
-		fileLocation, err := filepath.Abs("samples/workspaces/workspace_sample.json")
-
-		if err != nil {
-			Logger.Panic("Failed to get workspace devfile from location ", zap.Error(err))
-		}
-
-		file, err := ioutil.ReadFile(fileLocation)
-		if err != nil {
-			Logger.Panic("Failed to read workspace devfile ", zap.Error(err))
-		}
-
-		Logger.Info("Starting a new simple workspace")
-		_ = ctrl.RunWorkspace(file, workspaceStack)
-	})
-
-	ginkgo.It("Start NodeJS Workspace", func() {
-		workspaceStack := "nodejs"
-		httpClient, err := client.NewHttpClient()
-
-		ctrl := workspaces.NewWorkspaceController(httpClient)
-
-		fileLocation, err := filepath.Abs("samples/workspaces/workspace_nodejs.json")
-
-		if err != nil {
-			Logger.Panic("Failed to get workspace devFile from location ", zap.Error(err))
-		}
-
-		file, err := ioutil.ReadFile(fileLocation)
-		if err != nil {
-			Logger.Panic("Failed to read workspace devfile ", zap.Error(err))
-		}
-		Logger.Info("Starting a new NodeJS workspace")
-
-		_ = ctrl.RunWorkspace(file, workspaceStack)
-	})
-
 	ginkgo.It("Start java Maven Workspace", func() {
 		workspaceStack := "java-maven"
 		httpClient, err := client.NewHttpClient()
